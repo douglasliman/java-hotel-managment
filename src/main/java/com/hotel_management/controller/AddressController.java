7e6ac2478305122dd6a392d2a5f1010c2fe0b26c
Dafne Duda <dafne.leslie@gmail.com>
2024-07-24 15:40:52 -0300

    updates repository

---
+++ src/main/java/com/hotel_management/controller/AddressController.java
@@ -0,0 +1,37 @@
+package com.hotel_management.controller;
+
+import com.hotel_management.model.Address;
+import com.hotel_management.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Address>>getAllAddress(){
+        return ResponseEntity.ok(addressRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Address>createAddress(@RequestBody Address address){
+        Address savedAddress = addressRepository.save(address);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedAddress);
+    }
+}
+
+
+
+
+
+
+
+
--- src/main/java/com/hotel_management/model/Address.java
+++ src/main/java/com/hotel_management/model/Address.java
@@ -1,11 +1,14 @@
 package com.hotel_management.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -26,11 +29,6 @@ public class Address {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	
-	@Column(name = "country")
-	@NotNull(message = "Country is necessary")
-	@Size(min = 2, message = "min size is 2 characters")
-	String country;
-	
 	@Column(name = "state")
 	@NotNull(message = "State is necessary")
 	@Size(min = 2, message = "min size is 2 characters")
@@ -50,14 +48,15 @@ public class Address {
 	@NotNull(message = "Street is necessary")
 	@Size(min = 2, message = "min size is 2 characters")
 	String street;
-	
-	@Column(name = "zip_code")
-	@NotNull(message = "zip code is necessary")
-	@Size(min = 2, message = "min size is 2 characters")
-	int zipCode;
+
+	@NotNull(message = "Zip code is necessary")
+	@Min(value = 1000000, message = "Zip code must be a valid 7-digit number")
+	@Max(value = 99999999, message = "Zip code must be a valid 8-digit number")
+	@JsonProperty("zip_code")
+	private Integer zipCode;
 	
 	@Column(name = "Number")
 	@NotNull(message = "Number is necessary")
 	@Size(min = 2, message = "min size is 2 characters")
-	int number;
+	String number;
 }
